1a3189972263638e3c7f4e4f746b29f936e11589
Pierce Corcoran <pierce@plasticcow.com>
2019-01-11 16:00:40 -0800

    Basic laggy overview scrolling

--- griffon-app/models/games/thecodewarrior/bitfonteditor/OverviewModel.kt
+++ griffon-app/models/games/thecodewarrior/bitfonteditor/OverviewModel.kt
@@ -1,13 +1,32 @@
 package games.thecodewarrior.bitfonteditor
 
 import games.thecodewarrior.bitfont.file.BitfontBundle
-import griffon.core.artifact.GriffonModel;
+import games.thecodewarrior.bitfont.file.BlockFile
+import games.thecodewarrior.bitfont.file.UCDFile
+import griffon.core.artifact.GriffonModel
 import griffon.inject.MVCMember
-import griffon.metadata.ArtifactProviderFor;
-import org.codehaus.griffon.runtime.core.artifact.AbstractGriffonModel;
+import griffon.metadata.ArtifactProviderFor
+import org.codehaus.griffon.runtime.core.artifact.AbstractGriffonModel
+import java.util.TreeMap
 
 @ArtifactProviderFor(GriffonModel::class)
 class OverviewModel: AbstractGriffonModel() {
     @MVCMember
     lateinit var bundle: BitfontBundle
+
+    private val codepointMap: Map<UInt, UCDFile.Codepoint> by lazy {
+        bundle.ucd.codepoints.associateByTo(TreeMap()) {
+            it.codepoint
+        }
+    }
+
+    fun getInfo(codepoint: UInt): GlyphInfo {
+        return GlyphInfo(codepoint, null)// codepointMap[codepoint]
+    }
+
+    data class GlyphInfo(val codepoint: UInt, val glyph: BlockFile.Glyph?) {
+        companion object {
+            val NULL = GlyphInfo(0u, null)
+        }
+    }
 }
--- griffon-app/views/games/thecodewarrior/bitfonteditor/OverviewView.kt
+++ griffon-app/views/games/thecodewarrior/bitfonteditor/OverviewView.kt
@@ -1,20 +1,28 @@
 package games.thecodewarrior.bitfonteditor
 
 
+import games.thecodewarrior.bitfonteditor.util.CanvasWrapper
 import griffon.core.artifact.GriffonView
 import griffon.inject.MVCMember
 import griffon.metadata.ArtifactProviderFor
+import javafx.fxml.FXML
 import javafx.scene.Group
 import javafx.scene.Scene
-import javafx.scene.paint.Color
+import javafx.scene.canvas.Canvas
+import javafx.scene.control.ScrollPane
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.BorderPane
+import javafx.scene.layout.FlowPane
+import java.awt.Color
 import javafx.stage.DirectoryChooser
-import javafx.stage.FileChooser
 import javafx.stage.Stage
-import javafx.stage.Window
-import jdk.nashorn.tools.ShellFunctions.input
 import org.codehaus.griffon.runtime.javafx.artifact.AbstractJavaFXGriffonView
 import java.io.File
 import java.util.Collections.emptyMap
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.max
+import kotlin.properties.Delegates
 
 @ArtifactProviderFor(GriffonView::class)
 class OverviewView: AbstractJavaFXGriffonView() {
@@ -23,8 +31,18 @@ class OverviewView: AbstractJavaFXGriffonView() {
     @MVCMember
     lateinit var controller: OverviewController
 
+    @FXML
+    lateinit var rootPane: BorderPane
+    @FXML
+    lateinit var scrollPane: ScrollPane
+    @FXML
+    lateinit var anchorPane: AnchorPane
+    @FXML
+    lateinit var flowPane: FlowPane
+
     lateinit var fileChooser: DirectoryChooser
     lateinit var stage: Stage
+    lateinit var scene: Scene
 
     override fun initUI() {
         stage = getApplication().createApplicationContainer(emptyMap()) as Stage
@@ -40,8 +58,8 @@ class OverviewView: AbstractJavaFXGriffonView() {
 
     // build the UI
     private fun init(): Scene {
-        val scene = Scene(Group())
-        scene.fill = Color.WHITE
+        scene = Scene(Group())
+        scene.fill = javafx.scene.paint.Color.WHITE
         scene.stylesheets.add("bootstrapfx.css")
 
         val node = loadFromFXML()
@@ -50,10 +68,92 @@ class OverviewView: AbstractJavaFXGriffonView() {
         (scene.root as Group).children.addAll(node)
         connectActions(node, controller)
 
+        scene.heightProperty().addListener { _ -> resize() }
+        scene.widthProperty().addListener { _ -> resize() }
+
+        anchorPane.prefHeight = GlyphItem.ITEM_HEIGHT * 1000.0
+        scrollPane.vvalueProperty().addListener { _ ->
+            updateScroll()
+        }
+        scrollPane.widthProperty().addListener { _ ->
+            anchorPane.prefWidth = scrollPane.width
+            updateSize()
+        }
+        scrollPane.heightProperty().addListener { _ ->
+            updateSize()
+        }
+//        updateSize()
+//        currentStart = -1 // to force a refresh on the glyph items
+//        updateScroll()
         return scene
     }
 
     fun selectFile(): File? {
         return fileChooser.showDialog(stage)
+    }
+
+
+    private fun resize() {
+        rootPane.prefHeight = scene.height
+        rootPane.prefWidth = scene.width
+    }
+
+    var currentStart = 0
+    var columns = 1
+    var glyphItems = listOf<GlyphItem>()
+
+    fun updateScroll() {
+        val viewportTop = scrollPane.vvalue * (anchorPane.height - scrollPane.viewportBounds.height)
+        val topRow = max(0, floor(viewportTop / GlyphItem.ITEM_HEIGHT).toInt() - 1)
+        val start = topRow * columns
+        if(start != currentStart) {
+            AnchorPane.setTopAnchor(flowPane, GlyphItem.ITEM_HEIGHT.toDouble() * topRow)
+            currentStart = start
+            updateItems()
+        }
+    }
+
+    fun updateItems() {
+        glyphItems.forEachIndexed { i, it -> it.glyphInfo = model.getInfo((currentStart + i).toUInt()) }
+    }
+
+    fun updateSize() {
+        val viewport = scrollPane.viewportBounds
+        val columns = floor(flowPane.width / GlyphItem.ITEM_WIDTH).toInt()
+        val visibleRows = ceil(viewport.height / GlyphItem.ITEM_HEIGHT).toInt() + 4
+        if(columns == 0) return
+
+        if(glyphItems.size != columns * visibleRows) {
+            flowPane.children.clear()
+            glyphItems = List(columns * visibleRows) { GlyphItem(OverviewModel.GlyphInfo.NULL) }
+            flowPane.children.addAll(glyphItems)
+            updateItems()
+        }
+
+        if(columns != this.columns) {
+            this.columns = columns
+            val newRow = currentStart / columns
+
+            scrollPane.vvalue = (newRow * GlyphItem.ITEM_HEIGHT) / (anchorPane.height - viewport.height)
+        }
+    }
+
+    class GlyphItem(glyphInfo: OverviewModel.GlyphInfo): Canvas(ITEM_WIDTH.toDouble(), ITEM_HEIGHT.toDouble()) {
+        val wrapper = CanvasWrapper(this)
+        val infoLayer = wrapper.layer(0, ::drawInfo)
+        var glyphInfo by Delegates.observable(glyphInfo) { _, _, _ ->
+            infoLayer.redraw()
+        }
+
+        fun drawInfo(l: CanvasWrapper.Layer) {
+            l.g.color = Color(glyphInfo.hashCode() and 0xFFFFFF)
+            l.g.fillRect(1, 1, 48, 73)
+            l.g.drawString(glyphInfo.codepoint.toString(), 10, 30)
+        }
+
+        companion object {
+            const val ITEM_HEIGHT = 75
+            const val ITEM_WIDTH = 50
+        }
     }
 }
